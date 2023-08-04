4f2b503d5473ad0ae7c9aa12894a135ed19b5f96
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-08-04 15:41:47 -0700

    Clearing out of new classes to begin rework.

--- src/v2/Enemy.java
+++ src/v2/Enemy.java
@@ -1,49 +1,5 @@
 package v2;
-import processing.core.PImage;
 
 public class Enemy extends Main {
-    PImage sprite;
-
-    int x, y, speed;
-    public Enemy() {
-        this.sprite = loadImage("Images/Sprites/Enemy_Ship.png");
-        this.sprite.resize(64, 64);
-        this.x = (int)(Math.random() * (width - sprite.width));
-        this.y = (int)(Math.random() * -200) - 2000;
-        this.speed = (int)(Math.random() * 5) + 5;
-    }
-
-    public void act(Rocket rocket, Player player) {
-        this.move();
-        this.rocketContact(rocket);
-        this.hitChar(player);
-    }
-
-    public void move() {
-        this.y = this.y + this.speed;
-        if (this.y > height) {
-            this.respawn();
-        }
-    }
-
-    public void respawn() {
-        this.x = (int) (Math.random() * (width - this.sprite.width));
-        this.y = (int) (Math.random() * -100) - this.sprite.height;
-        this.speed = (int) (Math.random() * 5) + 5;
-    }
-
-    public void rocketContact(Rocket rocket) {
-        if (this.x < rocket.x && this.x + this.sprite.width > rocket.x && this.y + this.sprite.height < rocket.y) {
-            this.respawn();
-            rocket.fired = false;
-            hitScore = hitScore + 10;
-        }
-    }
-
-    public void hitChar(Player player) {
-        if (charContact(this.x, this.y, this.sprite.width, player.x, player.y, player.sprite.width)) {
-            player.health--;
-            this.respawn();
-        }
-    }
+    
 }
--- src/v2/Image.java
+++ src/v2/Image.java
@@ -1,14 +1,5 @@
 package v2;
-import processing.core.PImage;
 
 public class Image extends Main {
-    PImage sprite;
-    int x, y;
-    public Image(int x, int y, int length, int stature, String imageURL) {
-        this.x = x;
-        this.y = y;
-        this.sprite = loadImage(imageURL);
-        this.sprite.resize(length, stature);
-        startScreenImages.add(this);
-    }
+    
 }

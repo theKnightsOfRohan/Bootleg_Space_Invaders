7379f626ffed364a58d52b4b27ce66428751f44e
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-08-06 10:56:16 -0700

    Preliminary rocket class.

--- src/v2/Rocket.java
+++ src/v2/Rocket.java
@@ -3,7 +3,21 @@ package v2;
 import processing.core.PImage;
 
 public class Rocket extends Image {
-    public Rocket(PImage img) {
-        super(0, 0, 22, 32, img);
+    int speed, acc;
+
+    public Rocket(int x, int y, PImage img) {
+        super(x, y, 22, 32, img);
+        this.speed = 0;
+        this.acc = -1;
+    }
+
+    public void draw(Main main) {
+        this.move();
+        main.image(this.img, this.x, this.y);
+    }
+
+    private void move() {
+        this.speed += this.acc;
+        this.y += this.speed;
     }
 }
